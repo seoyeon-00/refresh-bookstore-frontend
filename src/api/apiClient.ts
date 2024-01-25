6816a7bd01210cc2037e2617537248682778c902
ksy <ksoup3434@gmail.com>
2024-01-26 01:48:34 +0900

    Fix:  Interceptor error 로직 수정

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,14 +1,9 @@
 import { getCookie } from "cookies-next";
 import { API_BASE_URL } from "@/constants/path";
 import axios, { AxiosError, AxiosInstance } from "axios";
-import { getUser, getUserData, requestToken } from "./auth";
-import { useContext, useEffect } from "react";
-import { AuthContext } from "@/contexts/AuthContext";
+import { getUserData, requestToken } from "./auth";
 
 export const apiClient = (): AxiosInstance => {
-  // let accessToken = localStorage.getItem("token");
-  // let refreshToken = getCookie("refresh-token");
-
   let accessToken =
     typeof window !== "undefined" ? localStorage.getItem("token") : null;
   let refreshToken =
@@ -28,51 +23,43 @@ export const apiClient = (): AxiosInstance => {
 
   api.interceptors.response.use(
     async (res) => {
-      const now = new Date();
+      return res;
+    },
+    async (error) => {
+      const originalRequest = error.config;
+      console.log("access 토큰 만료!");
+      console.log(error);
 
-      if (accessToken) {
-        const item = JSON.parse(accessToken);
-        if (now.getTime() > item.expires && refreshToken) {
-          console.log("access 토큰 만료!");
-          try {
-            const data = await getUserData();
-            console.log(data);
+      // api 요청 실패 시, refreshToken을 통해 accessToken 갱신
+      if (error) {
+        try {
+          const data = await getUserData();
+          console.log(data);
 
-            const refreshData = {
-              refreshToken: refreshToken,
-              email: data?.data.email,
-              password: data?.data.password,
-            };
+          const refreshData = {
+            refreshToken: refreshToken as string,
+          };
 
-            await requestToken(refreshData);
+          await requestToken(refreshData);
 
-            // 이전 요청을 다시 시도 (헤더에 새로운 액세스 토큰 추가)
-            if (res.config) {
-              const updatedAccessToken = localStorage.getItem("token");
-              if (updatedAccessToken) {
-                const updatedItem = JSON.parse(updatedAccessToken);
-                res.config.headers.Authorization = `Bearer ${updatedItem.value}`;
-                return axios(res.config);
-              } else {
-                console.error("Updated accessToken is null");
-              }
+          // 이전 요청을 다시 시도 (헤더에 새로운 액세스 토큰 추가)
+          if (originalRequest) {
+            const updatedAccessToken = localStorage.getItem("token");
+            if (updatedAccessToken) {
+              const updatedItem = JSON.parse(updatedAccessToken);
+              originalRequest.headers.Authorization = `Bearer ${updatedItem.value}`;
+              return axios(originalRequest);
             } else {
-              console.error("error.config is undefined");
+              console.error("Updated accessToken is null");
             }
-          } catch (error) {
-            // 재발급 실패 시 로그인 화면으로 리디렉션 또는 다른 처리
-            console.error("토큰 재발급 실패:", error);
-
-            // 여기서 리디렉션 또는 다른 처리를 수행할 수 있습니다.
+          } else {
+            console.error("error.config is undefined");
           }
+        } catch (error) {
+          console.error("토큰 재발급 실패:", error);
         }
       }
 
-      // 비동기 요청 성공 시 반환
-      return Promise.resolve(res);
-    },
-    async (error) => {
-      // 오류 응답 처리
       return Promise.reject(error);
     }
   );
